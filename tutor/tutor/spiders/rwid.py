e7e755e2207324c37f9e50206f71530149cfd6f4
Joko Susilo <susilo.j8@gmail.com>
2021-01-06 11:54:37 +0700

    setlogin params to website

--- tutor/tutor/spiders/rwid.py
+++ tutor/tutor/spiders/rwid.py
@@ -8,4 +8,18 @@ class RwidSpider(scrapy.Spider):
     start_urls = ['http://localhost:5000/']
 
     def parse(self, response):
+        params = {
+            "username": "user",
+            "password": "user12345"
+        }
+
+        return scrapy.FormRequest(
+            dont_filter=True,
+            url='http://localhost:5000/login',
+            formdata=params,
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
         yield {"title": response.css("title::text").get()}
+        pass
