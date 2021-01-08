2b6070bbe88b64c37043182c45b446c1f14aa97b
Joko Susilo <susilo.j8@gmail.com>
2021-01-08 10:36:18 +0700

    Add requirements.txt and parse detail image

--- tutor/tutor/spiders/rwid.py
+++ tutor/tutor/spiders/rwid.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
 import scrapy
+from cssselect import Selector
 
 
 class RwidSpider(scrapy.Spider):
     name = 'rwid'
+
     allowed_domains = ['localhost:5000']
     start_urls = ['http://localhost:5000/']
 
@@ -14,12 +18,34 @@ class RwidSpider(scrapy.Spider):
         }
 
         return scrapy.FormRequest(
-            dont_filter=True,
             url='http://localhost:5000/login',
             formdata=params,
-            callback=self.after_login
+            callback=self.after_login,
+            dont_filter=True,
         )
 
     def after_login(self, response):
-        yield {"title": response.css("title::text").get()}
-        pass
+        # get detail products
+        detail_products = response.css(".card .card-title a")
+        for detail_product in detail_products:
+            url_href = detail_product.attrib.get("href")
+            yield response.follow(url_href, callback=self.parse_detail, dont_filter=True)
+
+        # get pagination
+        paginations = response.css("ul.pagination a.page-link")
+        for pagination in paginations:
+            url_href = pagination.attrib.get("href")
+            yield response.follow(url_href, callback=self.after_login, dont_filter=True)
+
+    def parse_detail(self, response):
+        image = response.css(".card-img-top").attrib.get("src")
+        title = response.css(".card-title::text").get()
+        stock = response.css(".card-stock::text").get()
+        description = response.css(".card-text::text").get()
+
+        return {
+            "image": image,
+            "title": title,
+            "stock": stock,
+            "description": description
+        }
